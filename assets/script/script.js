6453eb1b9da4995575d456bf51ee107a21f2371b
cecyramos <c.e.c.y_92_@hotmail.com>
2025-07-31 16:50:24 -0400

    test de seguridad

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,3 +15,34 @@ botoncontacto.addEventListener("click", () => {
     alert("¡Gracias por tu mensaje!");
     }
 });
+//Test de Seguridad
+function mostrarTest() {
+    document.getElementById('modalTest').style.display = 'block'; // Mostrar el popup
+}
+function cerrarTest() {
+    document.getElementById('modalTest').style.display = 'none'; // Ocultar el popup
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const enviarRespuestasBtn = document.querySelector('.modal-content button[type="submit"]');
+
+    const respuestasCorrectas = {
+        pregunta1: 'pregunta1respuesta2',
+        pregunta2: 'pregunta2respuesta2',
+        pregunta3: 'pregunta3respuesta2'
+    };
+    if (enviarRespuestasBtn) {
+        enviarRespuestasBtn.addEventListener('click', procesarTest);
+    }
+    function procesarTest() {
+        let puntaje = 0;
+
+        for (const pregunta in respuestasCorrectas) {
+            const respuestaSeleccionada = document.querySelector(`input[name="${pregunta}"]:checked`);
+            if (respuestaSeleccionada && respuestaSeleccionada.id === respuestasCorrectas[pregunta]) {
+                puntaje++;
+            }
+        }
+
+        alert(`Tu puntaje es: ${puntaje} de ${Object.keys(respuestasCorrectas).length}`);
+    }
+});
